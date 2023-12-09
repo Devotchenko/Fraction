10aa7ef3b0bd3ff2130459cd8773909dee89f58d
Artem Devotchenko <eveldoctorer@gmail.com>
2023-12-09 08:28:16 +0300

    Добавьте файлы проекта.

---
+++ Fraction/Fraction.cpp
@@ -0,0 +1,23 @@
+﻿#include "Fraction.h"
+#include <iostream>
+
+int main() {
+    try {
+        Fraction f1(5, 10); 
+        Fraction f2(9, 3); 
+
+        std::cout << "f1 = " << f1 << ", f2 = " << f2 << std::endl;
+        std::cout << "f1 + f2 = " << f1 + f2 << std::endl;
+        std::cout << "f1 - f2 = " << f1 - f2 << std::endl;
+        std::cout << "f1 * f2 = " << f1 * f2 << std::endl;
+        std::cout << "f1 / f2 = " << f1 / f2 << std::endl;
+        std::cout << "f1 == f2: " << (f1 == f2 ? "true" : "false") << std::endl;
+        std::cout << "f1 != f2: " << (f1 != f2 ? "true" : "false") << std::endl;
+    }
+    catch (const std::invalid_argument& e) {
+        std::cerr << "An exception occurred: " << e.what() << std::endl;
+        return 1; 
+    }
+
+    return 0; 
+}
