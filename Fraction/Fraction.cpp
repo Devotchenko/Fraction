916a587e362c0660f06990cf059d3f142800cc5c
Evel Knievel <dr_ju@mail.ru>
2023-12-12 14:59:58 +0300

    срфтпув

--- Fraction/Fraction.cpp
+++ Fraction/Fraction.cpp
@@ -1,23 +1,78 @@
-﻿#include "Fraction.h"
-#include <iostream>
+#include "Fraction.h"
+#include <cmath>
 
-int main() {
-    try {
-        Fraction f1(5, 10); 
-        Fraction f2(9, 3); 
-
-        std::cout << "f1 = " << f1 << ", f2 = " << f2 << std::endl;
-        std::cout << "f1 + f2 = " << f1 + f2 << std::endl;
-        std::cout << "f1 - f2 = " << f1 - f2 << std::endl;
-        std::cout << "f1 * f2 = " << f1 * f2 << std::endl;
-        std::cout << "f1 / f2 = " << f1 / f2 << std::endl;
-        std::cout << "f1 == f2: " << (f1 == f2 ? "true" : "false") << std::endl;
-        std::cout << "f1 != f2: " << (f1 != f2 ? "true" : "false") << std::endl;
+int Fraction::gcd(int a, int b) const {
+    while (b != 0) {
+        int t = b;
+        b = a % b;
+        a = t;
     }
-    catch (const std::invalid_argument& e) {
-        std::cerr << "An exception occurred: " << e.what() << std::endl;
-        return 1; 
-    }
+    return a;
+}
 
-    return 0; 
+void Fraction::reduce() {
+    if (numerator == 0) {
+        denominator = 1;
+    }
+    else {
+        int gcd_value = gcd(std::abs(numerator), std::abs(denominator));
+        numerator /= gcd_value;
+        denominator /= gcd_value;
+        if (denominator < 0) {
+            denominator = -denominator;
+            numerator = -numerator;
+        }
+    }
+}
+
+Fraction::Fraction(int numerator, int denominator) : numerator(numerator), denominator(denominator) {
+    if (denominator == 0) {
+        throw std::invalid_argument("Denominator cannot be zero");
+    }
+    reduce();
+}
+
+Fraction Fraction::operator+(const Fraction& other) const {
+    Fraction result(numerator * other.denominator + other.numerator * denominator, denominator * other.denominator);
+    result.reduce();
+    return result;
+}
+
+Fraction Fraction::operator-(const Fraction& other) const {
+    Fraction result(numerator * other.denominator - other.numerator * denominator, denominator * other.denominator);
+    result.reduce();
+    return result;
+}
+
+Fraction Fraction::operator*(const Fraction& other) const {
+    Fraction result(numerator * other.numerator, denominator * other.denominator);
+    result.reduce();
+    return result;
+}
+
+Fraction Fraction::operator/(const Fraction& other) const {
+    if (other.numerator == 0) {
+        throw std::invalid_argument("Cannot divide by fraction with zero numerator");
+    }
+    Fraction result(numerator * other.denominator, denominator * other.numerator);
+    result.reduce();
+    return result;
+}
+
+bool Fraction::operator==(const Fraction& other) const {
+    return numerator * other.denominator == other.numerator * denominator;
+}
+
+bool Fraction::operator!=(const Fraction& other) const {
+    return !(*this == other);
+}
+
+std::ostream& operator<<(std::ostream& out, const Fraction& f) {
+    if (f.denominator == 1) {
+        out << f.numerator;
+    }
+    else {
+        out << f.numerator << "/" << f.denominator;
+    }
+    return out;
 }
--- Fraction/Fraction.h
+++ Fraction/Fraction.h
@@ -2,7 +2,6 @@
 #define FRACTION_H
 
 #include <iostream>
-#include <sstream>
 #include <stdexcept>
 
 class Fraction {
@@ -10,88 +9,20 @@ private:
     int numerator;
     int denominator;
 
-    // Функция для вычисления НОД
-    int gcd(int a, int b) const {
-        while (b != 0) {
-            int t = b;
-            b = a % b;
-            a = t;
-        }
-        return a;
-    }
-
-    // Метод для сокращения дроби
-    void reduce() {
-        if (numerator == 0) {
-            denominator = 1;  
-        }
-        else {
-            int gcd_value = gcd(numerator, denominator);
-            numerator /= gcd_value;
-            denominator /= gcd_value;
-        }
-    }
+    int gcd(int a, int b) const;
+    void reduce();
 
 public:
-    // Конструкторы
-    Fraction(int numerator = 0, int denominator = 1) : numerator(numerator), denominator(denominator) {
-        if (denominator == 0) {
-            throw std::invalid_argument("Denominator cannot be zero");
-        }
-        if (numerator == 0 && denominator != 1) {
-            throw std::invalid_argument("Zero numerator must have denominator as 1");
-        }
-        reduce();
-    }
-    // Арифметические операции
-    Fraction operator+(const Fraction& other) const {
-        Fraction result(numerator * other.denominator + other.numerator * denominator,
-            denominator * other.denominator);
-        result.reduce();
-        return result;
-    }
+    Fraction(int numerator = 0, int denominator = 1);
+    Fraction operator+(const Fraction& other) const;
+    Fraction operator-(const Fraction& other) const;
+    Fraction operator*(const Fraction& other) const;
+    Fraction operator/(const Fraction& other) const;
 
-    Fraction operator-(const Fraction& other) const {
-        Fraction result(numerator * other.denominator - other.numerator * denominator,
-            denominator * other.denominator);
-        result.reduce();
-        return result;
-    }
+    bool operator==(const Fraction& other) const;
+    bool operator!=(const Fraction& other) const;
 
-    Fraction operator*(const Fraction& other) const {
-        Fraction result(numerator * other.numerator, denominator * other.denominator);
-        result.reduce();
-        return result;
-    }
-
-    Fraction operator/(const Fraction& other) const {
-        if (other.numerator == 0) {
-            throw std::invalid_argument("Cannot divide by fraction with zero numerator");
-        }
-        Fraction result(numerator * other.denominator, denominator * other.numerator);
-        result.reduce();
-        return result;
-    }
-
-    // Сравнение
-    bool operator==(const Fraction& other) const {
-        return numerator * other.denominator == other.numerator * denominator;
-    }
-
-    bool operator!=(const Fraction& other) const {
-        return !(*this == other);
-    }
-
-    // Перегруженный оператор для вывода
-    friend std::ostream& operator<<(std::ostream& out, const Fraction& f) {
-        if (f.denominator == 1) {
-            out << f.numerator;
-        }
-        else {
-            out << f.numerator << "/" << f.denominator;
-        }
-        return out;
-    }
+    friend std::ostream& operator<<(std::ostream& out, const Fraction& f);
 };
 
 #endif // FRACTION_H
